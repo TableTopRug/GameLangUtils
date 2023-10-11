a83116f8f2eb30a9057f259451fd8d4fa3f07290
flame03fire <147567185+flame03fire@users.noreply.github.com>
2023-10-10 19:58:31 -0500

    Update GenerateColidersPNGColors.gd

--- GoDot/GenerateColidersPNGColors.gd
+++ GoDot/GenerateColidersPNGColors.gd
@@ -1,7 +1,8 @@
 extends Node
 
 # class member variables go here, for example:
-
+# switch to just holding the edge of the pixels that form the sides and check the 4 surrounding pixels
+# arround it to to figure out which the edge of the image is being outlined
 
 func _ready():
     # Called every time the node is added to the scene.
